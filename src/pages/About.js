6757f8da2a0fcd6cd80a1aac4de1e7e117228404
Grant Harris <grantxvx@gmail.com>
2020-12-11 22:06:56 -0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam culpa
+        quaerat assumenda laudantium eos laborum voluptatibus, quos consectetur
+        adipisci, tenetur, minima aliquam ea saepe vero maxime. Hic distinctio
+        dolores quisquam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
